e7670780b9372822b3d4fe4d5b971bb14f7777ed
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2021-02-05 09:32:47 +0530

    second commit

---
+++ build/precache-manifest.5ad6ec3b542954fa358dbc4edf62e676.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1f5f9102c769bd675c5564625c7b22",
+    "url": "/dispur-wireless/index.html"
+  },
+  {
+    "revision": "2a9b07b15b1cc71f6718",
+    "url": "/dispur-wireless/static/js/2.5cd434a2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/dispur-wireless/static/js/2.5cd434a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8500c12da99d3f94a21f",
+    "url": "/dispur-wireless/static/js/main.d3d7347a.chunk.js"
+  },
+  {
+    "revision": "eb59efe3a23f0a869413",
+    "url": "/dispur-wireless/static/js/runtime-main.555457f4.js"
+  },
+  {
+    "revision": "06ce697f0f10cc048e71c3bce9249a50",
+    "url": "/dispur-wireless/static/media/Group10.06ce697f.svg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/dispur-wireless/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/dispur-wireless/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "068b1c3456f23d56ea00acdb6893004b",
+    "url": "/dispur-wireless/static/media/svg-5.068b1c34.svg"
+  }
+]);
